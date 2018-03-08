3cf0891e0cc365bf7717e571774afab4b3941358
dl_carl@126.com <dl_carl@126.com>
2018-03-08 10:42:40 +0800

    first

---
+++ src/main/java/com/xiangnong/xnservice/XnServiceApplication.java
@@ -0,0 +1,12 @@
+package com.xiangnong.xnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XnServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XnServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xiangnong/xnservice/XnServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiangnong.xnservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XnServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
