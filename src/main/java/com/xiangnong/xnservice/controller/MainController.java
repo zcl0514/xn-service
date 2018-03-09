f4ea5e31f828f8df7d527cc88cd84f75027f1533
zcl <894431320@qq.com>
2018-03-09 16:39:01 +0800

    删除无效文件

--- src/main/java/com/xiangnong/xnservice/controller/MainController.java
+++ src/main/java/com/xiangnong/xnservice/controller/MainController.java
@@ -3,7 +3,6 @@ package com.xiangnong.xnservice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@RequestMapping("main")
 @Controller
 public class MainController {
 
